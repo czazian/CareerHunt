d38769486a04f6fc301979c251813900f2927bff
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2024-03-24 01:02:50 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/careerhunt/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.careerhunt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import com.example.careerhunt.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private val fragmentManager = supportFragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Set initial fragment
+        if (savedInstanceState == null) {
+            val transaction = fragmentManager.beginTransaction()
+            val initialFragment = JobListing()
+            transaction.replace(binding.frametLayout.id, initialFragment)
+            transaction.addToBackStack(null)
+            transaction.commit()
+        }
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+
+            when (item.itemId){
+                R.id.home -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = JobListing()
+                    transaction.replace(binding.frametLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+                R.id.explore -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = JobListing()
+                    transaction.replace(binding.frametLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+                R.id.interview -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = JobListing()
+                    transaction.replace(binding.frametLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+                R.id.alumni -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = JobListing()
+                    transaction.replace(binding.frametLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+                R.id.setting -> {
+                    val transaction = fragmentManager.beginTransaction()
+                    val fragment = Setting()
+                    transaction.replace(binding.frametLayout.id, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.commit()
+                }
+            }
+            true
+        }
+    }
+}
