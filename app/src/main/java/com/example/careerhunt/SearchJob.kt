c945fb344088bb65181c05816f9da06791a74882
ささやかな風と青い海 <99159488+czazian@users.noreply.github.com>
2024-03-25 23:41:44 +0800

    push

--- app/src/main/java/com/example/careerhunt/SearchJob.kt
+++ app/src/main/java/com/example/careerhunt/SearchJob.kt
@@ -1,10 +1,16 @@
 package com.example.careerhunt
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageButton
 
 class SearchJob : Fragment() {
 
@@ -13,6 +19,28 @@ class SearchJob : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_job, container, false)
+        val view = inflater.inflate(R.layout.fragment_search_job, container, false)
+
+        val bottomSheet = view.findViewById<ImageButton>(R.id.btnFilter)
+        bottomSheet.setOnClickListener() {
+            showDialog();
+        }
+
+        return view
+    }
+
+    fun showDialog() {
+        val dialog: Dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottom_sheet)
+
+        dialog.show()
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.window?.setBackgroundDrawableResource(R.drawable.bottom_sheet)
+        dialog.window?.attributes?.windowAnimations = R.style.BottomSheetAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
     }
 }
